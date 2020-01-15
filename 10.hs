02c9ad6cb75f4a700eb10a0c52c886f9237460c6
iambrj <joshibharathiramana@gmail.com>
2020-01-15 07:05:18 +0530

    Add 10

---
+++ 10.hs
@@ -0,0 +1,11 @@
+packIter :: (Eq a) => [a] -> [[a]] -> [[a]]
+packIter [] list = list
+packIter (first : rest) [] = packIter rest [[first]]
+packIter (first : rest) (firstEncode : restEncode) = case ((firstEncode !! 0) == first) of
+                                                         True -> packIter rest ([first : firstEncode] ++ restEncode)
+                                                         False -> packIter rest ([first] : firstEncode : restEncode)
+pack :: (Eq a) => [a] -> [[a]]
+pack list = reverse ((flip packIter) [] list)
+
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode x = map (\ list -> (length list, list !! 0)) (pack x)
