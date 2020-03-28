bf4fdaf7058e3b33de8006f410bb496e62bb88c0
iambrj <joshibharathiramana@gmail.com>
2020-03-28 15:22:24 +0530

    Add better solution to 15

--- 15.hs
+++ 15.hs
@@ -4,4 +4,8 @@ explode a n = a : (explode a (n - 1))
 
 repli :: [a] -> Int -> [a]
 repli [] _ = []
-repli (x : xs) count = (explode x count) ++ (repli xs count)
+repli list count = concatMap (flip(explode) count) list
+
+replicator :: [a] -> Int -> [a]
+replicator [] _ = []
+replicator list count = concatMap (\s -> s <$ [1..count]) list
